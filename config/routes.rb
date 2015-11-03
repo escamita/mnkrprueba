1e7242328ff0c9b86c143fae3ed2144b7ceced4a
alfredo <alfredoelozano@gmail.com>
2015-11-03 16:26:01 +0000

    testing branch development

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,13 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-
   get 'welcome/about'
-
   get 'welcome/contact'
-
   get 'welcome/faq'
-
   get 'welcome/pricing'
-
   get 'welcome/features'
-
+  
   resources :invoices
-
+  
   root to: 'welcome#index'
 
 end
